b0537523311d8aaca4333cfff95a03b717aac4a2
Muhammad Ibrahim <ibrahimaq99@gmail.com>
2023-09-16 05:37:06 +0500

    Top Nav added | Intro section added | Animared cursor added

---
+++ src/app/Components/TopNav.js
@@ -0,0 +1,47 @@
+"use client";
+
+import Image from "next/image";
+
+const navItems = [
+  {
+    name: "About",
+    href: "/about",
+  },
+  {
+    name: "Projects",
+    href: "/projects",
+  },
+  {
+    name: "Conact",
+    href: "/contact",
+  },
+];
+
+export default function TopNav() {
+  return (
+    <section className="bg-white/70 border-b border-black/5 rounded-b-lg sticky top-0 backdrop-blur-2xl">
+      <div className="w-full max-w-7xl lg:px-24 md:px-12 px-8 py-6 flex justify-between mx-auto">
+        {/* Logo */}
+        <Image
+          src="/vercel.svg"
+          alt="Vercel Logo"
+          className="dark:invert"
+          width={100}
+          height={24}
+        />
+        {/* Navigation */}
+        <nav className="flex gap-5">
+          {navItems.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className="text-black font-medium hover:text-accent-400 py-2 text-sm px-2"
+            >
+              {item.name}
+            </a>
+          ))}
+        </nav>
+      </div>
+    </section>
+  );
+}
