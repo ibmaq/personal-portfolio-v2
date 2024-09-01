1037da8e130f44980ad24565d82f488042f8a6b1
ibrahim-agit <ibrahim.aginfotech@gmail.com>
2024-09-01 19:58:14 +0500

    metadata updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,16 +4,35 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Muhammad Ibrahim",
-  description: "Ibrahim - Frontend Developer",
+  title: "Frontend Developer - JavaScript, React, Vue, TailwindCSS | Ibrahim",
+  description:
+    "Experienced Frontend Developer specializing in JavaScript, React, Vue, and TailwindCSS. Check out my portfolio!",
+  openGraph: {
+    title: "Frontend Developer - JavaScript, React, Vue, TailwindCSS | Ibrahim",
+    description:
+      "Experienced Frontend Developer specializing in JavaScript, React, Vue, and TailwindCSS. Check out my portfolio!",
+    url: "https://muhammadibrahim.com",
+    images: [
+      {
+        url: "/m_ibrahim.jpg",
+        width: 800,
+        height: 600,
+        alt: "Ibrahim's Portfolio",
+      },
+    ],
+  },
+  twitter: {
+    title: "Frontend Developer - JavaScript, React, Vue, TailwindCSS | Ibrahim",
+    description:
+      "Experienced Frontend Developer specializing in JavaScript, React, Vue, and TailwindCSS. Check out my portfolio!",
+    image: "/m_ibrahim.jpg",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>
-        {children}
-      </body>
+      <body className={`${inter.className}`}>{children}</body>
     </html>
   );
 }
